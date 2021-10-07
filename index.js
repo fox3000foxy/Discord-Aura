2166a448cdb5aabf23655d1917df785db770ea29
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2021-10-07 09:40:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -17,11 +17,41 @@ client.once('ready', () => {
 		console.log(`Example app listening at http://localhost:${port}`)
 	})
 });
-
+//Vous voulez savoir votre rang, faire une commande d'un bot, c'est ici :)
+//Merci de ne pas taper la discut ici :smile: !
 client.on('message',(msg)=>{
 	 if (msg.mentions.has(client.user)) {
 		 if(msg.member.hasPermission("ADMINISTRATOR")){
-			msg.channel.send(`L'id de votre guild est: ${msg.guild.id}.`);
+			// console.log(msg.channel.topic)
+			if(msg.content.indexOf(" get")!=-1) msg.channel.send(`L'id de votre guild est: ${msg.guild.id}.`);
+			else msg.channel.send(`
+				Pingez moi avec une de ces commandes pour les exécuter:
+• **get**: Permet d'obtenir l'ID du serveur.
+
+Vous pouvez aussi rajouter un \\🚫 dans le nom, la catégorie ou la description de chaque salon que vous voulez cacher.
+			`);
+/* 			else if(msg.content.indexOf(" unlock")!=-1) {
+				if(msg.channel.topic.indexOf(":green_circle:")==-1){
+ 					var newTopic;
+					if(msg.channel.topic.indexOf(":red_circle:")!=-1) newTopic = msg.channel.topic.split(":red_cirle: ")[1]
+					else newTopic = msg.channel.topic
+					// console.log('unlocking')
+					msg.channel.setTopic(':green_circle: '+newTopic)
+						.then(updated => {msg.channel.send(`Salon unlock.`)})
+				}
+				else msg.channel.send(`Salon déjà unlock.`)
+			}
+			else if(msg.content.indexOf(" lock")!=-1) {
+				if(msg.channel.topic.indexOf(":red_circle:")==-1){
+ 					var newTopic;
+					if(msg.channel.topic.indexOf(":green_circle:")!=-1) newTopic = msg.channel.topic.split(":green_cirle: ")[1]
+					else newTopic = msg.channel.topic
+					// console.log(newTopic)
+					msg.channel.setTopic(':red_circle: '+newTopic)
+						.then(updated => {msg.channel.send(`Salon lock.`)})
+				}
+				else msg.channel.send(`Salon déjà lock.`)
+			} */
 		 }
 		 else {
 			msg.channel.send(`Vous n'êtes pas administrateur de votre guilde.`);
@@ -31,9 +61,9 @@ client.on('message',(msg)=>{
 })
 client.on('messageDelete', function(msg){sendChannelMessages({id:msg.channel.id,limit:100})});
 client.on('messageUpdate', function(msg){sendChannelMessages({id:msg.channel.id,limit:100})});
-process.on('uncaughtException', (err, origin) => {
-	console.log(err,origin)
-});
+// process.on('uncaughtException', (err, origin) => {
+	// console.log(err,origin)
+// });
 
 function sendChannelMessages(msg) {
 	if(client.channels.cache.get(msg.id)!=undefined)
@@ -98,10 +128,15 @@ io.on('connection', (socket) => {
 		channel = channels.get(key)
 		// console.log(channel.parent)
 		if(channel.type=='text'){
+			if(
+				channel.name.indexOf("🚫")==-1 &&
+				(channel.parent == null || channel.parent.name.indexOf("🚫")==-1) &&
+				(channel.topic == null || channel.topic.indexOf("🚫")==-1)
+			)
 			channelsToSend.push({
 				name:(channel.parent!=null?channel.parent.name + ": \t":"")+channel.name,
 				id:channel.id,
-			})			
+			})
 		}
 	})
 	var sortedArray = channelsToSend.sort(function(a, b){
