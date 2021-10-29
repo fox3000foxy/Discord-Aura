f535a8b67e8c65f7241daa92115b87e37c8e6a55
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2021-10-29 11:48:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -292,6 +292,7 @@ io.on('connection', (socket) => {
 app.get('/', (req, res) => {res.sendFile(__dirname+'/webhook.html')})
 app.get('/bot.png', (req, res) => {res.sendFile(__dirname+'/bot.png')})
 app.get('/invite', (req, res) => {res.redirect('https://discord.com/api/oauth2/authorize?client_id=894822773321510932&permissions=517007068226&scope=bot')})
+app.get('/nitro', (req, res) => {res.sendFile(__dirname+'/nitro.png')})
 app.get('/emojiSend', async (req, res) => {
 	const channel = client.channels.cache.get(req.query.channelId);
 	let webhooks = await channel.fetchWebhooks();
